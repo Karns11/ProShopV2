598297887b222f81a3a702702b96ce51b63b857f
nathan karns <Nathan k>
2023-05-30 22:06:16 -0400

    set up auth middleware and logout functionality

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -5,11 +5,11 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  let statusCode = res.statusCode === 200 ? 500 : res.StatusCode;
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
   //Check for Mongoose bad ObjectId
-  if (err.name === "CastError" && err.kind === "ObkectId") {
+  if (err.name === "CastError" && err.kind === "ObjectId") {
     message = "Resource not found";
     statusCode = 404;
   }
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
 import express from "express";
 import dontenv from "dotenv";
+import cookieParser from "cookie-parser";
 dontenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //this is for the form url enocded on postman
+
+//Cookie parser middleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
